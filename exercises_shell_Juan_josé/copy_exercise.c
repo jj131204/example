0b990443ff1640a5df6307cc4127be29eba3b0a1
jj131204 <jj131204@gmail.com>
2021-04-07 02:01:12 +0000

    Error (se queda en  un bucle)

---
+++ exercises_shell_Juan_josé/copy_exercise.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#define SEPARATORS " ,!?\'\"\n\t"
+
+int ejecutar(char **ar);
+void token(char *n, char **tok);
+
+int main()
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+	int num;
+	char *tok[100];
+
+	while(1)
+	{
+		printf("$");
+		num = getline(&line, &bufsize, stdin);
+		if (num == EOF)
+		{
+			return (0);
+		}
+		printf("%s", line);
+	
+		token(line, tok);
+	
+		ejecutar(tok);
+
+	}
+
+	return(0);
+}
+
+void token(char *n, char **tok)
+{
+	char *token;
+	int count = 0;
+
+	token = strtok(n, SEPARATORS);
+	tok[count] = token;
+	for (count = 1; token != NULL; count++)
+	{
+		token = strtok(NULL, SEPARATORS);
+		tok[count] = token;
+	}
+
+
+}
+
+int ejecutar(char **tok)
+{
+
+pid_t pid;
+int status;
+
+pid = fork();
+
+	if(pid == 0)
+	{	
+	if (execve(tok[0], tok, NULL) == -1)
+	{
+		perror("Error:");
+	}
+		return (1);
+	}
+
+	else if (pid < 0)
+	{
+		perror("Error");
+		return(0);
+	}
+	else
+	{
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		}while (WIFEXITED(status) && !WIFSIGNALED(status));
+
+
+	}
+	return(1);
+
+
+}
